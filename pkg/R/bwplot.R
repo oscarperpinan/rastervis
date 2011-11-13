7d61e9ec6515515c10587c35985d0aea4d69a091
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-13 20:48:15 +0000

    first draft of bwplot method for formula, Raster

--- pkg/R/bwplot.R
+++ pkg/R/bwplot.R
@@ -40,3 +40,54 @@ setMethod('bwplot',
           )
 
 
+## setMethod('bwplot', signature(x='formula', data='Raster'),
+##           definition=function(x, data, dirXY, maxpixels=1e5,
+##             xscale.components=xscale.raster,
+##             yscale.components=yscale.raster,
+##             horizontal=FALSE,
+##             violin=TRUE,
+##             par.settings=rasterTheme,...){
+
+##             localSets = list(box.rectangle=list(col='black'),
+##               plot.symbol = list(pch='.', cex = 0.1))
+
+##             if (is.list(par.settings)){
+##               par.settings <- modifyList(par.settings, localSets)
+##             } else if (is.function(par.settings)){
+##               par.settings <- modifyList(par.settings(), localSets)
+##             } else par.settings <- localSets
+                
+
+##             nms <- layerNames(data)
+##             nl <- nlayers(data)
+
+##             data <- sampleRegular(data, maxpixels, asRaster=TRUE)
+##             df <- getValues(data)
+##             df <- as.data.frame(df)
+##             names(df) <- make.names(nms)
+
+##             xLayer <- getValues(init(data, v='x'))
+##             yLayer <- getValues(init(data, v='y'))
+
+##             df <- cbind(data.frame(x=xLayer, y=yLayer), df)
+
+##             if (!missing(dirXY)) {
+##               dirXY <- getValues(xyLayer(data, dirXY=substitute(dirXY)))
+##               df <- cbind(df, dirXY)
+##             }
+
+##             p <- bwplot(x=x, data=df,
+##                         horizontal=horizontal,
+##                         xscale.components=xscale.components,
+##                         yscale.components=yscale.components,                            
+##                         par.settings=par.settings, ...,
+##                         panel = function(..., box.ratio) {
+##                           if (violin) {panel.violin(..., col = "lightblue",
+##                                                     varwidth = FALSE, box.ratio = box.ratio)}
+##                           panel.bwplot(..., col='black',
+##                                        cex=0.8, pch='|', fill='gray', box.ratio = .1)
+##                         }
+##                         )
+##             p
+##           }
+##             )
