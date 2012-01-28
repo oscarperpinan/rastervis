8ef822998a47ed1b6a4d2ba9809ebf21cc86e310
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-28 11:10:33 +0000

    melt is not needed anymore in gplot. Example inside dontrun

--- pkg/R/gplot.R
+++ pkg/R/gplot.R
@@ -10,20 +10,29 @@ if (!isGeneric("gplot")) {
 }	
 
 setMethod("gplot", signature(x='Raster'), 
-	function(x, maxpixels=50000, ...)  {
-		stopifnot(require(ggplot2))
-		nl <- nlayers(x)
-		if (ncell(x) > maxpixels) {
-			x <- sampleRegular(x, maxpixels, asRaster=TRUE)
-		}
-		if (nl == 1) {
-			x <- data.frame(xyFromCell(x, 1:ncell(x)), value=values(x))
-			x <- melt(x, id.vars = c('x','y'), na.rm = TRUE)
-		} else {
-			x <- data.frame(xyFromCell(x, 1:ncell(x)), values(x))
-			x <- melt(x, id.vars = c('x','y'), na.rm = TRUE)
-		}
-		ggplot(aes(x=x, y=y), data=x, ...) 
-	}
-)
+          function(x, maxpixels=50000, ...)  {
+            stopifnot(require(ggplot2))
+            nl <- nlayers(x)
+            if (ncell(x) > maxpixels) {
+              x <- sampleRegular(x, maxpixels, asRaster=TRUE)
+            }
+
+            ## if (nl == 1) {
+            ## 	x <- data.frame(xyFromCell(x, 1:ncell(x)), value=values(x))
+            ## 	x <- melt(x, id.vars = c('x','y'), na.rm = TRUE)
+            ## } else {
+            ## 	x <- data.frame(xyFromCell(x, 1:ncell(x)), values(x))
+            ## 	x <- melt(x, id.vars = c('x','y'), na.rm = TRUE)
+            ## }
+
+            coords <- xyFromCell(x, seq_len(ncell(x)))
+            ## Extract values 
+            dat <- stack(as.data.frame(getValues(x)))
+            names(dat) <- c('value', 'variable')
+
+            dat <- cbind(coords, dat)
+
+            ggplot(aes(x=x, y=y), data=dat, ...) 
+          }
+          )
 
