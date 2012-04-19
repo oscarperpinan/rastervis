1508546c84d4f40df1aa4db52d845ab7fa4eda05
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-19 12:27:24 +0000

    The xyplot method only works for Raster* whose z slot is not NULL: add a stop line and warning in docs.

--- pkg/R/xyplot.R
+++ pkg/R/xyplot.R
@@ -11,7 +11,10 @@ setMethod('xyplot',
           definition=function(x, data=NULL, dirXY=y,
             xlab='Time', ylab='', digits=0,
             par.settings=rasterTheme,...){
+
             idx=getZ(x)
+            if (is.null(idx)) stop('z slot of the object is NULL.')
+
             dirLayer <- xyLayer(x, dirXY=substitute(dirXY))
             z <- zonal(x, dirLayer, mean, digits=digits)
             nRows <- nrow(z)
