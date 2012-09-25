65db89565622ef74b3c407299c697164a2554099
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-25 12:28:14 +0000

    contourplot: scales added as internal element of the list

--- pkg/R/contourplot.R
+++ pkg/R/contourplot.R
@@ -11,27 +11,29 @@ setMethod('contourplot',
             ...){
             dots <- list(...)
             settings <- list(
-                             margin=TRUE, FUN.margin=mean, scales.margin = NULL, 
-                             maxpixels=1e5,            
-                             between=list(x=0.5, y=0.2),
-                             as.table=TRUE,
-                             xlab='', ylab='', main='',
-                             xscale.components=xscale.raster,
-                             yscale.components=yscale.raster,
-                             zscaleLog = NULL, 
-                             cuts=7,
-                             labels = TRUE,
-                             contour = TRUE,
-                             pretty = TRUE,
-                             region = FALSE,
-                             colorkey = FALSE
-                             )
+                          margin=TRUE, FUN.margin=mean,
+                          scales=list(),
+                          scales.margin = NULL, 
+                          maxpixels=1e5,            
+                          between=list(x=0.5, y=0.2),
+                          as.table=TRUE,
+                          xlab='', ylab='', main='',
+                          xscale.components=xscale.raster,
+                          yscale.components=yscale.raster,
+                          zscaleLog = NULL, 
+                          cuts=7,
+                          labels = TRUE,
+                          contour = TRUE,
+                          pretty = TRUE,
+                          region = FALSE,
+                          colorkey = FALSE
+                          )
             call <- modifyList(settings, list(...))
             call$x <- x
             call$data <- NULL
             if (!missing(layers)) call$layers <- layers
             p <- do.call(levelplot, call)
             p
-            }
+          }
           )
 
