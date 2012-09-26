d6af9c785b291119a5287b95391bc07f17f877cf
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-26 12:46:27 +0000

    levelplot displays categorical data

--- pkg/R/levelplot.R
+++ pkg/R/levelplot.R
@@ -8,7 +8,7 @@ setGeneric('levelplot')
 setMethod('levelplot',
           signature(x='Raster', data='missing'),
           definition=function(x, data=NULL, layers,
-            margin=TRUE, FUN.margin=mean, 
+            margin=!(any(is.factor(x))), FUN.margin=mean, 
             maxpixels=1e5,
             par.settings=rasterTheme,
             between=list(x=0.5, y=0.2),
@@ -27,7 +27,10 @@ setMethod('levelplot',
             if (!missing(layers)) {
               object <- subset(x, subset=layers)
             } else {object <- x}
-  
+
+            ## Is factor?
+            isFactor <- all(is.factor(object)) ## TODO: different individual layers
+
             dat <- sampleRegular(object, size=maxpixels, asRaster=TRUE)
             nms <- make.names(layerNames(dat))
 
@@ -37,9 +40,10 @@ setMethod('levelplot',
             ## Extract values 
             dat <- getValues(dat)
 
+            
             ## If zscale is not NULL, transform the values and choose a function
             ## to calculate the labels
-            if (!is.null(zscaleLog)){
+            if (!is.null(zscaleLog) && !isFactor){
               zlogbase <- if (is.logical(zscaleLog)) {
                 10
               } else {
@@ -91,7 +95,7 @@ setMethod('levelplot',
               
             } else { ## !isLonLat
               aspect='iso'
-              }
+            }
 
             if (region==FALSE) colorkey=FALSE
            
@@ -101,7 +105,7 @@ setMethod('levelplot',
             has.contour <- (contour==TRUE) ##(!missing(contour) && contour==TRUE)
 
             ## Build the zscale.components and colorkey paying attention to zscaleLog
-            if (!is.null(zscaleLog)){
+            if (!is.null(zscaleLog) && !isFactor){
               zscale <- zscale.components(zlim, zlogbase)
               if (has.colorkey){
                 colorkey.default=list(labels=zscale, raster=TRUE, interpolate=TRUE)
@@ -125,11 +129,28 @@ setMethod('levelplot',
                 )
               if (has.colorkey){## put the colorkey at the bottom to leave space for the margin
                 if (is.logical(colorkey)){
-                  colorkey=list(space='bottom')                
+                  colorkey=list(space='bottom')
                 } else {
                   colorkey=modifyList(colorkey, list(space='bottom'))
                 }}
             }
+
+            if (isFactor && has.colorkey) {
+              rat <- data.frame(levels(object)[[1]])
+              classes <- rat[,2] #TODO: allow a different column
+              nClasses <- nlevels(factor(rat$ID))
+              rng <- range(rat$ID)
+              ## define the breaks of the color key
+              my.at <- seq(rng[1]-1, rng[2])
+              ## the labels will be placed vertically centered
+              my.labs.at <- seq(rng[1], rng[2])-0.5
+              colorkey <- modifyList(colorkey,
+                                     list(
+                                       at=my.at,
+                                       labels=
+                                          list(labels=classes, ## classes names as labels
+                                               at=my.labs.at)))
+              }
 
             ## Build the formula for levelplot
             form <- as.formula(paste(paste(names(df)[-c(1,2)], collapse='+'), 'x*y', sep='~'))
@@ -146,7 +167,7 @@ setMethod('levelplot',
                                    lwd=c(1, 0.4) # Two line widths, for main and minor contour lines
                                    ){
               ##Draw the regions with the panel requested by the user
-              if (region)levelPanel(..., at=at, contour=FALSE, labels=FALSE)
+              if (region) levelPanel(..., at=at, contour=FALSE, labels=FALSE)
               has.labels <- (is.logical(labels) && labels) || is.list(labels)
               mainTicks=(as.character(zscale$labels)!=FALSE)
               mainLabels <- list(labels=zscale$labels[mainTicks], cex=0.8)
@@ -174,6 +195,7 @@ setMethod('levelplot',
                 if (length(names.attr) != nlayers(object))
                   stop('Length of names.attr should match number of layers.')
                 }
+            
             p <- levelplot(form, data=df,
                            scales=scales,
                            aspect=aspect,
@@ -187,7 +209,7 @@ setMethod('levelplot',
                            contour=contour, region=region, labels=labels,
                            strip=strip.custom(factor.levels=names.attr),
                            ## The panel depends on zscaleLog and contour
-                           panel=if (!is.null(zscaleLog) && has.contour) {
+                           panel=if (!is.null(zscaleLog) && has.contour && !isFactor) {
                              panelMixed 
                            } else {
                              requestedPanel 
