82ad305450c30852d546b2833469bdd4ef20b74a
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-04 00:48:52 +0000

    Added reverse and maxpixels (different from narrows) in vectorplot

--- pkg/R/vectorplot.R
+++ pkg/R/vectorplot.R
@@ -8,8 +8,8 @@ setMethod('vectorplot',
           signature(object='Raster'),
           definition = function(object, layers,
             narrows=2e3, lwd.arrows=0.6, length=unit(5e-2, 'npc'),
-            region=TRUE,
-            isField=FALSE, unit='radians',
+            maxpixels=1e5, region=TRUE,  
+            isField=FALSE, reverse=FALSE, unit='radians',
             ...){
             if (!missing(layers)) {
               object <- subset(object, subset=layers)
@@ -20,7 +20,7 @@ setMethod('vectorplot',
             y <- getValues(init(dat, v='y'))
 
 
-            fooSlopeAspect <- function(s, skip, unit){
+            fooSlopeAspect <- function(s, skip, unit, reverse){
               aspX <- xres(s)*0.6 ## maybe use a non-constant value...
               aspY <- yres(s)*0.6
 
@@ -30,41 +30,57 @@ setMethod('vectorplot',
               slope <- getValues(subset(s, 1)) 
               aspect <- getValues(subset(s, 2))
               if (unit=='degrees') {aspect <- aspect/180*pi}
+              if (reverse) aspect <- aspect + pi
+
     
               slope <- scale(slope, center=FALSE) ##center=FALSE to get only positive values of slope
               dx <- slope*sin(aspect)*aspX ##sin due to the angular definition of aspect
               dy <- slope*cos(aspect)*aspY
 
-              data.frame(dx, dy)
+              x <- getValues(init(s, v='x'))
+              y <- getValues(init(s, v='y'))
+
+              data.frame(x, y, dx, dy)
             }
 
             if (nlayers(object)>1 && !isField){ ##slopeAspect works only for RasterLayer
-              sa <- lapply(unstack(dat), fooSlopeAspect, skip=FALSE, unit=unit)
+              sa <- lapply(unstack(dat), fooSlopeAspect, skip=FALSE, unit=unit, reverse=reverse)
               sa <- do.call(rbind, sa) 
             } else {
-              sa <- fooSlopeAspect(dat, skip=isField, unit=unit)
+              sa <- fooSlopeAspect(dat, skip=isField, unit=unit, reverse=reverse)
             }
 
-            fooPanel <- function(x, y, z, subscripts, sa, length, lwd.arrows,...) {
-              if (region) {panel.levelplot.raster(x, y, z, subscripts, interpolate=TRUE,...)}
-              xs <- x[subscripts]
-              ys <- y[subscripts]
-              zs <- z[subscripts]
-              ## Although sa does not store the subscripts information,
-              ## it is in the same order so it is safe to use it this way
-              sas <- sa[subscripts,]
-              panel.arrows(xs, ys, xs+sas$dx, ys+sas$dy, 
-                           length=length,
-                           lwd=lwd.arrows)
-            }
+            ## fooPanel <- function(x, y, z, subscripts, sa, length, lwd.arrows,...) {
+            ##   ## if (region) {panel.levelplot.raster(x, y, z, subscripts, interpolate=TRUE,...)}
+            ##   xs <- x[subscripts]
+            ##   ys <- y[subscripts]
+            ##   ## zs <- z[subscripts]
+
+            ##   ## Although sa does not store the subscripts information,
+            ##   ## it is in the same order so it is safe to use it this way
+            ##   sas <- sa[subscripts,]
+            ##   panel.arrows(xs, ys, xs+sas$dx, ys+sas$dy, 
+            ##                length=length,
+            ##                lwd=lwd.arrows)
+            ## }
+
             if (isField) object <- subset(object, 1) ##only uses the magnitude for the region
-            p <- levelplot(object, maxpixels=narrows, 
-                           colorkey=region, 
-                           sa=sa, length=length,
-                           lwd.arrows=lwd.arrows,
-                           panel=fooPanel, ...)
-            p
-  
+            
+              
+
+            ## p <- levelplot(object, maxpixels=narrows, 
+            ##                colorkey=FALSE,  
+            ##                sa=sa, length=length,
+            ##                lwd.arrows=lwd.arrows,
+            ##                panel=fooPanel, ...)
+            
+            levelplot(object, maxpixels=maxpixels, region=region,...) +
+              xyplot(y~x, data=sa, dx=sa$dx, dy=sa$dy,
+                     length=length, lwd.arrows=lwd.arrows,
+                     panel=function(x, y, dx, dy, length, lwd.arrows, ...){
+                       panel.arrows(x, y, x+dx, y+dy,
+                                    length=length, lwd=lwd.arrows, ...)
+                     })
           }
           )
 
