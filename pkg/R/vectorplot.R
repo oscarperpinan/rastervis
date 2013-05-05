3d9abca0f764a2d5434e2fa7cd13ca547fb8dd89
oscarperpi <oscarperpi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-05 10:13:14 +0000

    aspect is converted to radians only if skip is TRUE

--- pkg/R/vectorplot.R
+++ pkg/R/vectorplot.R
@@ -8,7 +8,7 @@ setMethod('vectorplot',
           signature(object='Raster'),
           definition = function(object, layers,
             narrows=2e3, lwd.arrows=0.6, length=unit(5e-2, 'npc'),
-            maxpixels=1e5, region=TRUE,  
+            maxpixels=1e5, region=TRUE,
             isField=FALSE, reverse=FALSE, unit='radians',
             ...){
             if (!missing(layers)) {
@@ -24,15 +24,15 @@ setMethod('vectorplot',
               aspX <- xres(s)*0.6 ## maybe use a non-constant value...
               aspY <- yres(s)*0.6
 
-              if (!skip){s <- terrain(s, opt=c('slope', 'aspect'))} 
+              if (!skip){s <- terrain(s, opt=c('slope', 'aspect'))}
               ##If s is a vector field, the first layer is the magnitude (slope)
               ##and the second is the angle (aspect)
-              slope <- getValues(subset(s, 1)) 
+              slope <- getValues(subset(s, 1))
               aspect <- getValues(subset(s, 2))
-              if (unit=='degrees') {aspect <- aspect/180*pi}
+              if (unit=='degrees' & skip) {aspect <- aspect/180*pi}
               if (reverse) aspect <- aspect + pi
 
-    
+
               slope <- scale(slope, center=FALSE) ##center=FALSE to get only positive values of slope
               dx <- slope*sin(aspect)*aspX ##sin due to the angular definition of aspect
               dy <- slope*cos(aspect)*aspY
@@ -45,7 +45,7 @@ setMethod('vectorplot',
 
             if (nlayers(object)>1 && !isField){ ##slopeAspect works only for RasterLayer
               sa <- lapply(unstack(dat), fooSlopeAspect, skip=FALSE, unit=unit, reverse=reverse)
-              sa <- do.call(rbind, sa) 
+              sa <- do.call(rbind, sa)
             } else {
               sa <- fooSlopeAspect(dat, skip=isField, unit=unit, reverse=reverse)
             }
@@ -59,21 +59,21 @@ setMethod('vectorplot',
             ##   ## Although sa does not store the subscripts information,
             ##   ## it is in the same order so it is safe to use it this way
             ##   sas <- sa[subscripts,]
-            ##   panel.arrows(xs, ys, xs+sas$dx, ys+sas$dy, 
+            ##   panel.arrows(xs, ys, xs+sas$dx, ys+sas$dy,
             ##                length=length,
             ##                lwd=lwd.arrows)
             ## }
 
             if (isField) object <- subset(object, 1) ##only uses the magnitude for the region
-            
-              
 
-            ## p <- levelplot(object, maxpixels=narrows, 
-            ##                colorkey=FALSE,  
+
+
+            ## p <- levelplot(object, maxpixels=narrows,
+            ##                colorkey=FALSE,
             ##                sa=sa, length=length,
             ##                lwd.arrows=lwd.arrows,
             ##                panel=fooPanel, ...)
-            
+
             levelplot(object, maxpixels=maxpixels, region=region,...) +
               xyplot(y~x, data=sa, dx=sa$dx, dy=sa$dy,
                      length=length, lwd.arrows=lwd.arrows,
