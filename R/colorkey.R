1d2aace6af8d786a3e8f31228c63daf4b104ea1c
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2020-05-13 18:11:01 +0200

    Fixes title placement after changes in lattice, closes #59
    Solved thanks to the help of Deepayan Sarkar, as detailed in issue deepayan/lattice#18

--- R/colorkey.R
+++ R/colorkey.R
@@ -2,47 +2,46 @@ drawCK <- function(key, space = 'right')
 {
     ## Get the grob produced by draw.colorkey
     ck <- draw.colorkey(key)
-    ## Define the graphical parameter using the axis.text values as default
+    ## Define the graphical parameter using the axis.text values as
+    ## default
     if (is.null(key$title.gpar)) key$title.gpar <- list()
     title.gpar <- modifyList(trellis.par.get("axis.text"),
                              key$title.gpar)
     title.gpar <- do.call(gpar, title.gpar)
     ## Add the title in a suitable position, depending on the location
-    ## of the colorkey (defined by "space").  Although draw.colorkey
-    ## defines a layout with three rows/cols, but two of them are
-    ## empty (reserved?), I am not able to use the free
-    ## rows/cols. Instead, using the row/col occupied by the color bar
-    ## with packGrob, and respecting the height/width with
-    ## grobHeight/grobWidth, I get correct results
+    ## of the colorkey (defined by "space").  
+    title.grob <-
+        switch(space,
+               right = textGrob(key$title,
+                                x = 0,
+                                y = unit(1, 'npc') + unit(0.6, 'lines'),
+                                just = 'left', gp = title.gpar),
+               left = textGrob(key$title,
+                               x = 1,
+                               y = unit(1, 'npc') + unit(0.6, 'lines'),
+                               just = 'right', gp = title.gpar),
+               bottom = textGrob(key$title,
+                                 y = 0.5,
+                                 x = unit(0, 'npc') - unit(0.6, 'lines'),
+                                 just = 'right', gp = title.gpar),
+               top = textGrob(key$title,
+                              y = 0.5,
+                              x = unit(0, 'npc') - unit(0.6, 'lines'),
+                              just = 'right', gp = title.gpar),
+               )
+    zw <- unit(0, "mm")
     ck <- switch(space,
-                 right = packGrob(ck,
-                                  textGrob(key$title,
-                                           x = 0,
-                                           y = unit(1, 'npc') + unit(0.6, 'lines'),
-                                           just = 'left', gp = title.gpar),
-                                  row = 2, col = 3,
-                                  height = grobHeight(ck)),
-                 left = packGrob(ck,
-                                 textGrob(key$title,
-                                          x = 1,
-                                          y = unit(1, 'npc') + unit(0.6, 'lines'),
-                                          just = 'right', gp = title.gpar),
-                                 row = 2, col = 1,
-                                 height = grobHeight(ck)),
-                 bottom = packGrob(ck,
-                                   textGrob(key$title,
-                                            y = 0.5,
-                                            x = unit(0, 'npc') - unit(0.6, 'lines'),
-                                            just = 'right', gp = title.gpar),
-                                   row = 3, col = 2,
-                                   width = grobWidth(ck)),
-                 top = packGrob(ck,
-                                textGrob(key$title,
-                                         y = 0.5,
-                                         x = unit(0, 'npc') - unit(0.6, 'lines'),
-                                         just = 'right', gp = title.gpar),
-                                row = 1, col = 2,
-                                width = grobWidth(ck))
-                 )
+                 right = packGrob(ck, title.grob,
+                                  row = 1, col = 3,
+                                  height = zw),
+                 left = packGrob(ck, title.grob,
+                                 row = 1, col = 1,
+                                 height = zw),
+                 bottom = packGrob(ck, title.grob,
+                                   row = 3, col = 1,
+                                   width = zw),
+                 top = packGrob(ck, title.grob,
+                                row = 1, col = 1,
+                                width = zw))
     ck
 }
