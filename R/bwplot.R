c9f3e72b9e72f6ed702b0abd51eed5d494fc22a4
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2020-12-14 12:06:56 +0100

    Modify bwplot for terra

--- R/bwplot.R
+++ R/bwplot.R
@@ -2,52 +2,65 @@ setGeneric("bwplot", function(x, data, ...)
            standardGeneric("bwplot"))
 
 setMethod('bwplot',
-          signature(x='RasterStackBrick', data='missing'),
+          signature(x='RSBoT', data='missing'),
           definition=function(x, data = NULL, layers, FUN,
             maxpixels = 1e+05,
-            xlab='', ylab='', main='',
+            xlab = '', ylab = '', main = '',
             violin = TRUE, draw.points = FALSE, do.out = FALSE,
             par.settings = bwTheme(),
             violin.ratio = 1, box.ratio = 0.5,
             scales = list(x = list(rot = 45, cex = 0.8)),
-            ...) {
-            if (!missing(layers)) x <- subset(x, layers)
-            nl=nlayers(x)
-            if (nl > 1) {
-              dat <- raster2dat(x, FUN, maxpixels)
-              bwplot(values~ind,
-                     data = dat, 
-                     xlab = xlab, ylab = ylab, main = main,
-                     horizontal = FALSE,
-                     violin.ratio = violin.ratio, box.ratio = box.ratio,
-                     do.out = do.out,
-                     panel = function(..., violin.ratio, box.ratio, do.out) {
-                         
-                         if (isTRUE(violin))
-                             panel.violin(...,
-                                          varwidth = FALSE,
-                                          box.ratio = violin.ratio)
+            ...)
+          {
 
-                         if (isTRUE(draw.points))
-                             panel.stripplot(...,
-                                             jitter.data = TRUE,
-                                             factor = 2 * box.ratio)
-                         panel.bwplot(...,
-                                      pch='|', do.out = do.out,
-                                      box.ratio = box.ratio)
-                         
-                     },
-                     par.settings = par.settings,
-                     scales=scales
-                     )
-            } else {
-              stop('bwplot is defined only for Raster objects with two or more layers.')
+              if (!missing(layers)) x <- subset(x, layers)
+
+              ## Number of layers
+              if (is(x, "SpatRaster"))
+                  nly  <- nlyr(x)
+              else
+                  nly  <- nlayers(x)
+              
+              if (nly > 1)
+              {
+                  dat <- raster2dat(x, FUN, maxpixels)
+                  bwplot(values~ind,
+                         data = dat, 
+                         xlab = xlab, ylab = ylab, main = main,
+                         horizontal = FALSE,
+                         violin.ratio = violin.ratio,
+                         box.ratio = box.ratio,
+                         do.out = do.out,
+                         panel = function(...,
+                                          violin.ratio,
+                                          box.ratio,
+                                          do.out) {
+                             
+                             if (isTRUE(violin))
+                                 panel.violin(...,
+                                              varwidth = FALSE,
+                                              box.ratio = violin.ratio)
+                             
+                             if (isTRUE(draw.points))
+                                 panel.stripplot(...,
+                                                 jitter.data = TRUE,
+                                                 factor = 2 * box.ratio)
+                             panel.bwplot(...,
+                                          pch='|', do.out = do.out,
+                                          box.ratio = box.ratio)
+                             
+                         },
+                         par.settings = par.settings,
+                         scales=scales
+                         )
+              } else {
+                  stop('bwplot is defined only for Raster objects with two or more layers.')
               }
           }
           )
 
 
-setMethod('bwplot', signature(x='formula', data='Raster'),
+setMethod('bwplot', signature(x='formula', data='RoT'),
           definition=function(x, data, dirXY, maxpixels = 1e+05,
             xscale.components = xscale.raster,
             yscale.components = yscale.raster,
@@ -57,48 +70,81 @@ setMethod('bwplot', signature(x='formula', data='Raster'),
             par.settings = bwTheme(),
             ...){
 
+              ## Names of layers
               nms <- names(data)
+              ## Ensure valid names
+              nms <- make.names(nms, unique = TRUE)
               
-              nl <- nlayers(data)
-              
-              data <- sampleRegular(data, maxpixels, asRaster=TRUE)
-              df <- getValues(data)
+              ## Number of layers
+              if (is(data, "SpatRaster"))
+                  nly  <- nlyr(data)
+              else
+                  nly  <- nlayers(data)
+
+              if (is(data, "SpatRaster"))
+              {
+                  dataSample <- spatSample(data,
+                                           size = maxpixels,
+                                           as.raster = TRUE)
+                  df <- values(dataSample)
+                  xLayer <- values(xLayer)
+                  yLayer <- values(yLayer)
+              }
+              else 
+                {
+                    dataSample <- sampleRegular(data,
+                                                size = maxpixels,
+                                                asRaster = TRUE)
+                    df <- getValues(dataSample)
+                    xLayer <- getValues(xLayer)
+                    yLayer <- getValues(yLayer)
+                }
+                  
               df <- as.data.frame(df)
-              names(df) <- make.names(nms)
-              
-              xLayer <- getValues(init(data, v='x'))
-              yLayer <- getValues(init(data, v='y'))
+              names(df) <- nms
               
               df <- cbind(data.frame(x=xLayer, y=yLayer), df)
               
-              if (!missing(dirXY)) {
-                  dirXY <- getValues(xyLayer(data, dirXY=substitute(dirXY)))
+              if (!missing(dirXY))
+              {
+                  dirXY <- xyLayer(dataSample,
+                                   dirXY = substitute(dirXY))
+
+                  if (is(data, "Raster"))
+                      dirXY <- getValues(dirXY)
+                  else
+                      dirXY <- values(dirXY)
+
                   df <- cbind(df, dirXY)
               }
               
               p <- bwplot(x = x, data = df,
                           horizontal=horizontal,
                           xscale.components=xscale.components,
-                          yscale.components=yscale.components,                            
-                          violin.ratio = violin.ratio, box.ratio = box.ratio,
+                          yscale.components=yscale.components,
+                          violin.ratio = violin.ratio,
+                          box.ratio = box.ratio,
                           do.out = do.out,
-                     panel = function(..., violin.ratio, box.ratio, do.out) {
-                         
-                         if (isTRUE(violin))
-                             panel.violin(...,
-                                          varwidth = FALSE,
-                                          box.ratio = violin.ratio)
-
-                         if (isTRUE(draw.points))
-                             panel.stripplot(...,
-                                             jitter.data = TRUE,
-                                             factor = 2 * box.ratio)
-                         panel.bwplot(...,
-                                      pch='|', do.out = do.out,
-                                      box.ratio = box.ratio)
-                         
-                     },
-                     par.settings=par.settings, ...)
+                          panel = function(...,
+                                           violin.ratio,
+                                           box.ratio,
+                                           do.out) {
+                              
+                              if (isTRUE(violin))
+                                  panel.violin(...,
+                                               varwidth = FALSE,
+                                               box.ratio = violin.ratio)
+                              
+                              if (isTRUE(draw.points))
+                                  panel.stripplot(...,
+                                                  jitter.data = TRUE,
+                                                  factor = 2 * box.ratio)
+                              panel.bwplot(...,
+                                           pch='|', do.out = do.out,
+                                           box.ratio = box.ratio)
+                              
+                          },
+                          par.settings=par.settings, ...)
               p
           })
 
