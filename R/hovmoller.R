7387452d38dced59c3fc91efa12c5740a06e30d3
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2021-01-21 16:51:04 +0100

    Remove xyLayer code from hovmoller.R

--- R/hovmoller.R
+++ R/hovmoller.R
@@ -1,14 +1,3 @@
-globalVariables('y')
-
-##Create a Layer from a custom function of the coordinates
-xyLayer <- function(object, dirXY=y){
-  y <- init(object, fun='y')
-  x <- init(object, fun='x')
-  isLanguage <- try(is.language(dirXY), silent=TRUE)
-  if (class(isLanguage)=='try-error' || !isLanguage) dirXY <- substitute(dirXY)
-  dirLayer <- eval(dirXY)
-}
-
 ##Hovmoller diagram
 ##http://en.wikipedia.org/wiki/Hovm%C3%B6ller_diagram
 
