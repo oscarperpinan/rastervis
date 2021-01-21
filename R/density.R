b6cfca9869ecc0680a802b0a20b74d67d1dcdf47
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2021-01-21 16:49:36 +0100

    histogram and densityplot for terra

--- R/density.R
+++ R/density.R
@@ -3,107 +3,101 @@ globalVariables('ind')
 setGeneric('densityplot')
 
 setMethod('densityplot',
-          signature(x='RasterLayer', data='missing'),
-          definition=function (x, data=NULL, maxpixels = 1e+05,
-            xlab='', ylab='', main='', col='black',...){
-            dat <- raster2dat(x, maxpixels=maxpixels)
-            densityplot(dat,
-                        data = NULL,
-                        pch = '.', col = col,
-                        xlab = xlab, ylab = ylab, main = main,
-                        ...) 
-          }
-          )
-
-  
-setMethod('densityplot',
-          signature(x='RasterStackBrick', data='missing'),
-          definition=function (x, data=NULL, layers, FUN,
+          signature(x='RoT', data='missing'),
+          definition=function (x, data = NULL, layers, FUN,
             maxpixels = 1e+05,
             xlab = '', ylab = '', main = '',
-            par.settings=rasterTheme(),
+            par.settings = rasterTheme(),
             draw.labels = TRUE,
             auto.key = list(space = "right"),
-            ...){
-            if (!missing(layers)) x <- subset(x, layers)
-            nl=nlayers(x)
-            if (nl > 1) {
-                dat <- raster2dat(x, FUN, maxpixels)
-                if (draw.labels == TRUE)
-                    p <- densityplot(~values,
-                                     data = dat, groups = ind,
-                                     breaks = 100,
-                                     par.settings = par.settings, pch = '.',
-                                     xlab = xlab, ylab = ylab, main = main,
-                                     panel = panel.superpose,
-                                     panel.groups = function(x, group.value, col.line,...){
-                                         panel.densityplot(x,
-                                                           col.line = col.line,
-                                                           plot.points = FALSE,
-                                                           ...)
-                                         d <- density(x, na.rm = TRUE)
-                                         i <- which.max(d$y)
-                                         ltext(d$x[i],d$y[i],
-                                               group.value,
-                                               adj = c(0.3,0),
-                                               col = col.line,
-                                               cex = 0.7)
-                                         
-
-                                     })
-                
-                else
-                    p <- densityplot(~values,
-                                     data = dat, groups = ind,
-                                     breaks = 100,
-                                     par.settings = par.settings, pch = '.',
-                                     xlab = xlab, ylab = ylab, main = main,
-                                     auto.key = auto.key, ...)                
-            }
-            else
-                {
-                    p <- densityplot(x,
-                                     maxpixels = maxpixels,
-                                     main = main, xlab=xlab, ylab=ylab,
-                                     ...)
-                }
-            p
+            att = 1,
+            ...)
+          {
+              if (!missing(layers))
+                  x <- subset(x, layers)
+              
+              if (is(x, "SpatRaster"))
+                  nly  <- nlyr(x)
+              else
+                  nly  <- nlayers(x)
+              
+              if (nly > 1) {
+                  dat <- raster2dat(x, FUN, maxpixels, att)
+                  if (draw.labels == TRUE)
+                      p <- densityplot(~values,
+                                       data = dat, groups = ind,
+                                       breaks = 100,
+                                       par.settings = par.settings, pch = '.',
+                                       xlab = xlab, ylab = ylab, main = main,
+                                       panel = panel.superpose,
+                                       panel.groups = function(x, group.value, col.line,...){
+                                           panel.densityplot(x,
+                                                             col.line = col.line,
+                                                             plot.points = FALSE,
+                                                             ...)
+                                           d <- density(x, na.rm = TRUE)
+                                           i <- which.max(d$y)
+                                           ltext(d$x[i],d$y[i],
+                                                 group.value,
+                                                 adj = c(0.3,0),
+                                                 col = col.line,
+                                                 cex = 0.7)
+                                       })
+                  
+                  else ##draw.labels = FALSE
+                      p <- densityplot(~values,
+                                       data = dat, groups = ind,
+                                       breaks = 100,
+                                       par.settings = par.settings, pch = '.',
+                                       xlab = xlab, ylab = ylab, main = main,
+                                       auto.key = auto.key, ...)                
+              } 
+              else
+              { ## Number of layers = 1
+                  dat <- raster2dat(x, maxpixels = maxpixels, att = att)
+                  p <- densityplot(dat,
+                                   data = NULL,
+                                   par.settings = par.settings,
+                                   pch = '.',
+                                   xlab = xlab, ylab = ylab, main = main,
+                                   ...) 
+              }
+              p
           }
           )
 
                  
 
-setMethod('densityplot', signature(x='formula', data='Raster'),
+setMethod('densityplot', signature(x='formula', data='RoT'),
           definition=function(x, data, dirXY, maxpixels=1e+05,
             xscale.components=xscale.raster,
             yscale.components=yscale.raster,
             auto.key = list(space = 'right'), 
             par.settings=rasterTheme(),...){
 
-            isFactor <- which(is.factor(data))
-            levelsData <- levels(data)[[isFactor]][[1]][,2]
 
-            if (!missing(dirXY)) {
-              dirXY <- xyLayer(data, dirXY=substitute(dirXY))
-              names(dirXY) <- 'dirXY'
-              data <- stack(data, dirXY)
-            }
+              if (is(data, "Raster"))
+              {
+                  dataSample <- sampleRegular(data,
+                                              size = maxpixels,
+                                              asRaster = TRUE)
+                  df <- as.data.frame(dataSample, xy = TRUE)
+                  
+              } else
+              {
+                  idx <- spatSample(data,
+                                    size = maxpixels,
+                                    cells = TRUE)
+                  vals <- data[idx]
+                  xy <- xyFromCell(data, idx)
+                  df <- cbind(xy, vals)
+              }
 
-            df <- as.data.frame(sampleRegular(data,
-                                              maxpixels,
-                                              xy=TRUE))
-
-            ## Categorical data
-            if (any(isFactor)){
-               df[, isFactor + 2] <- as.factor(
-                   levelsData[df[, isFactor + 2]])
-                  }
-
-            p <- densityplot(x=x, data=df,
-                             xscale.components=xscale.components,
-                             yscale.components=yscale.components,
+            p <- densityplot(x = x, data = df,
+                             xscale.components = xscale.components,
+                             yscale.components = yscale.components,
                              auto.key = auto.key, 
-                             par.settings=par.settings, ...)
+                             par.settings = par.settings, ...)
             p
           }
           )
--- R/histogram.R
+++ R/histogram.R
@@ -1,93 +1,132 @@
-setGeneric('histogram')
+setGeneric('histogram', function(x, data, ...)
+           standardGeneric("histogram"))
 
 setMethod('histogram',
-          signature(x='RasterLayer', data='missing'),
-          definition=function (x, data=NULL, maxpixels = 1e+05, nint=100,
-            xlab='', ylab='', main='', col='gray',...){
-            dat <- raster2dat(x, maxpixels=maxpixels)
-            p <- histogram(dat,
-                           data=NULL,
-                           nint=nint, col=col,
-                           xlab=xlab, ylab=ylab,
-                           main=main, ...)
-            p
-          }
-)  
-
-
-setMethod('histogram',
-          signature(x='RasterStackBrick', data='missing'),
-          definition=function (x, data=NULL, layers, FUN,
-            maxpixels = 1e+05, nint=100,
-            xlab='', ylab='', main='', col='gray',
-            between=list(x=0.5, y=0.2),
-            as.table=TRUE,
-            scales=list(x=list(relation='free'),
-              y=list(relation='free',
-                     draw=FALSE)),
+          signature(x='RoT', data = 'missing'),
+          definition=function (x, data = NULL, layers, FUN,
+            maxpixels = 1e+05, nint = 100,
+            xlab = '', ylab='', main = '', col = 'gray',
+            between = list(x=0.5, y=0.2),
+            as.table = TRUE,
+            scales=list(x = list(relation = 'free'),
+              y=list(relation = 'free',
+                     draw = FALSE)),
             names.attr,
-            par.settings=rasterTheme(),
-            ...) {
-            if (!missing(layers)) x <- subset(x, layers)
-            nl=nlayers(x)
-            if (nl > 1) {
-                ## Names of each panel
-                if (missing(names.attr)){
-                    names.attr <- names(x)
-                } else {
-                    ## Do not coerce with as.character to allow formulas 
-                    if (length(names.attr) != nlayers(x))
-                        stop('Length of names.attr should match number of layers.')
-                }
+            par.settings = rasterTheme(),
+            att = 1,
+            ...)
+          {
+              if (!missing(layers))
+                  x <- subset(x, layers)
 
-              dat <- raster2dat(x, FUN, maxpixels)
-              p <- histogram(~values|ind, data = dat,
-                             as.table = as.table,
-                             par.settings = par.settings,
-                             between = between,
-                             scales = scales,
-                             nint = nint, col = col,
-                             xlab = xlab, ylab = ylab, main = main,
-                             strip = strip.custom(factor.levels = names.attr),
+              if (is(x, "SpatRaster"))
+                  nly  <- nlyr(x)
+              else
+                  nly  <- nlayers(x)
 
-                             ...)
-            } else {
-              p <- histogram(x, maxpixels = maxpixels, nint = nint,
-                             main = main, ylab = ylab, xlab = xlab, col = col,...)
-            }
-            p
-          }
-          )
-
-setMethod('histogram', signature(x='formula', data='Raster'),
-          definition=function(x, data, dirXY, maxpixels=1e+05,
-              strip=TRUE, par.settings=rasterTheme(),
-              ...){
-
-              isFactor <- which(is.factor(data))
-              levelsData <- levels(data)[[isFactor]][[1]][,2]
-
-              if (!missing(dirXY)) {
-                  dirXY <- xyLayer(data, dirXY=substitute(dirXY))
-                  names(dirXY) <- 'dirXY'
-                  data <- stack(data, dirXY)
-              }
-
-              df <- as.data.frame(sampleRegular(data,
-                                                maxpixels,
-                                                xy=TRUE))
-              ## Categorical data
-              if (any(isFactor)){
-                  df[, isFactor + 2] <- as.factor(
-                      levelsData[df[, isFactor + 2]])
-                  if (isTRUE(strip)) {
-                      strip <- strip.custom(strip.levels=TRUE)
+              if (nly > 1)
+              {## Multilayer Objects
+                  if (missing(names.attr)){
+                      nms <- names(x)
+                      ## Ensure valid names
+                      names.attr <- make.names(nms, unique = TRUE)
+                  } else {
+                      ## Do not coerce with as.character to allow formulas 
+                      if (length(names.attr) != nlayers(x))
+                          stop('Length of names.attr should match number of layers.')
                   }
+                  
+                  dat <- raster2dat(x, FUN, maxpixels=maxpixels, att=att)
+                  p <- histogram(~values|ind, data = dat,
+                                 as.table = as.table,
+                                 par.settings = par.settings,
+                                 between = between,
+                                 scales = scales,
+                                 nint = nint, col = col,
+                                 xlab = xlab, ylab = ylab, main = main,
+                                 strip = strip.custom(factor.levels = names.attr),
+
+                                 ...)
+              } else
+              {
+                  dat <- raster2dat(x, maxpixels=maxpixels, att=att)
+                  p <- histogram(dat,
+                                 data = NULL,
+                                 nint = nint, col = col,
+                                 xlab = xlab, ylab = ylab,
+                                 main = main, ...)
+              }
+              p
+          })
+
+setMethod('histogram', signature(x='formula', data='RoT'),
+          definition = function(x, data,
+                                dirXY,
+                                maxpixels = 1e+05,
+                                strip = TRUE,
+                                par.settings = rasterTheme(),
+                                att = 1,
+                                ...)
+          {
+
+              ## Names of layers
+              nms <- names(data)
+              ## Ensure valid names
+              nms <- make.names(nms, unique = TRUE)
+              
+              ## Number of layers
+              if (is(data, "SpatRaster"))
+                  nly  <- nlyr(data)
+              else
+                  nly  <- nlayers(data)
+
+              if (is(data, "SpatRaster"))
+              {
+                  dataSample <- spatSample(data,
+                                           size = maxpixels,
+                                           as.raster = TRUE)
+                  xLayer <- init(dataSample, fun='x')
+                  yLayer <- init(dataSample, fun='y')
+
+                  df <- values(dataSample)
+                  xLayer <- values(xLayer)
+                  yLayer <- values(yLayer)
+              }
+              else 
+                {
+                    dataSample <- sampleRegular(data,
+                                                size = maxpixels,
+                                                asRaster = TRUE)
+                    xLayer <- init(dataSample, fun='x')
+                    yLayer <- init(dataSample, fun='y')
+
+                    df <- getValues(dataSample)
+                    xLayer <- getValues(xLayer)
+                    yLayer <- getValues(yLayer)
+                }
+                  
+              df <- as.data.frame(df)
+              names(df) <- nms
+              
+              df <- cbind(data.frame(x=xLayer, y=yLayer), df)
+              
+              if (!missing(dirXY))
+              {
+                  dirXY <- xyLayer(dataSample,
+                                   dirXY = substitute(dirXY))
+
+                  if (is(data, "Raster"))
+                      dirXY <- getValues(dirXY)
+                  else
+                      dirXY <- values(dirXY)
+
+                  df <- cbind(df, dirXY)
               }
 
-              p <- histogram(x=x, data=df,
-                             strip=strip,
-                             par.settings=par.settings, ...)
+              p <- histogram(x = x, data = df,
+                             strip = strip,
+                             par.settings = par.settings,
+                             ...)
               p
           }
           )
