d1841da0416723288623bca17f34e701981be097
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2021-01-24 20:22:16 +0100

    Change densityplot wihout virtual class

--- R/dataframe.R
+++ R/dataframe.R
@@ -1,42 +1,20 @@
 dfRegular <- function(data, maxpixels)
 {
-    ## Names of layers
-    nms <- names(data)
-    ## Ensure valid names
-    nms <- make.names(nms, unique = TRUE)
-    
-    
-    if (is(data, "SpatRaster"))
+    if (is(data, "Raster"))
     {
-        nly  <- nlyr(data)
-        dataSample <- spatSample(data,
-                                 size = maxpixels,
-                                 as.raster = TRUE)
-        xLayer <- init(dataSample, fun='x')
-        yLayer <- init(dataSample, fun='y')
-        
-        df <- values(dataSample)
-        xLayer <- values(xLayer)
-        yLayer <- values(yLayer)
-    }
-    else 
-    {
-        nly  <- nlayers(data)
         dataSample <- sampleRegular(data,
                                     size = maxpixels,
                                     asRaster = TRUE)
-        xLayer <- init(dataSample, fun='x')
-        yLayer <- init(dataSample, fun='y')
+        df <- as.data.frame(dataSample, xy = TRUE)
         
-        df <- getValues(dataSample)
-        xLayer <- getValues(xLayer)
-        yLayer <- getValues(yLayer)
+    } else
+    {
+        idx <- spatSample(data,
+                          size = maxpixels,
+                          cells = TRUE)
+        vals <- data[idx]
+        xy <- xyFromCell(data, idx)
+        df <- cbind(xy, vals)
     }
-    
-    df <- as.data.frame(df)
-    
-    df <- cbind(data.frame(x=xLayer, y=yLayer), df)
-    names(df) <- c("x", "y", nms)
-    
     df
 }
--- R/density.R
+++ R/density.R
@@ -2,8 +2,76 @@ globalVariables('ind')
 
 setGeneric('densityplot')
 
+########################################################################
+## densityplot for data
+########################################################################
+## Only one layer
+densityLayer <- function(x,
+                         maxpixels,
+                         xlab, ylab, main,
+                         par.settings, draw.labels, auto.key,
+                         att,
+                         ...)
+{
+    dat <- raster2dat(x, maxpixels = maxpixels, att = att)
+    p <- densityplot(dat,
+                     data = NULL,
+                     par.settings = par.settings,
+                     pch = '.',
+                     xlab = xlab, ylab = ylab, main = main,
+                     ...) 
+    p
+}
+
+## Multilayer objects
+densityMLayer <- function(x,
+                          layers, FUN,
+                          maxpixels,
+                          xlab, ylab, main,
+                          par.settings, draw.labels, auto.key,
+                          att,
+                          ...)
+{
+    if (!missing(layers))
+        x <- subset(x, layers)
+    
+    dat <- raster2dat(x, FUN, maxpixels, att)
+    if (draw.labels == TRUE)
+        p <- densityplot(~values,
+                         data = dat, groups = ind,
+                         breaks = 100,
+                         par.settings = par.settings, pch = '.',
+                         xlab = xlab, ylab = ylab, main = main,
+                         panel = panel.superpose,
+                         panel.groups = function(x, group.value,
+                                                 col.line, ...)
+                         {
+                             panel.densityplot(x,
+                                               col.line = col.line,
+                                               plot.points = FALSE,
+                                               ...)
+                             d <- density(x, na.rm = TRUE)
+                             i <- which.max(d$y)
+                             ltext(d$x[i],d$y[i],
+                                   group.value,
+                                   adj = c(0.3,0),
+                                   col = col.line,
+                                   cex = 0.7)
+                         })
+    
+    else ##draw.labels = FALSE
+        p <- densityplot(~values,
+                         data = dat, groups = ind,
+                         breaks = 100,
+                         par.settings = par.settings, pch = '.',
+                         xlab = xlab, ylab = ylab, main = main,
+                         auto.key = auto.key, ...)                
+
+    p
+}
+
 setMethod('densityplot',
-          signature(x='RoT', data='missing'),
+          signature(x='Raster', data='missing'),
           definition=function (x, data = NULL, layers, FUN,
             maxpixels = 1e+05,
             xlab = '', ylab = '', main = '',
@@ -13,91 +81,93 @@ setMethod('densityplot',
             att = 1,
             ...)
           {
-              if (!missing(layers))
-                  x <- subset(x, layers)
               
-              if (is(x, "SpatRaster"))
-                  nly  <- nlyr(x)
+              if (nlayers(x) > 1)
+                  densityMLayer(x,
+                                layers, FUN,
+                                maxpixels,
+                                xlab, ylab, main,
+                                par.settings, draw.labels, auto.key,
+                                att,
+                                ...)
               else
-                  nly  <- nlayers(x)
+                  densityLayer(x,
+                               maxpixels,
+                               xlab, ylab, main,
+                               par.settings, draw.labels, auto.key,
+                               att,
+                               ...)
+          })
+
+
+setMethod('densityplot',
+          signature(x='SpatRaster', data='missing'),
+          definition=function (x, data = NULL, layers, FUN,
+            maxpixels = 1e+05,
+            xlab = '', ylab = '', main = '',
+            par.settings = rasterTheme(),
+            draw.labels = TRUE,
+            auto.key = list(space = "right"),
+            att = 1,
+            ...)
+          {
               
-              if (nly > 1) {
-                  dat <- raster2dat(x, FUN, maxpixels, att)
-                  if (draw.labels == TRUE)
-                      p <- densityplot(~values,
-                                       data = dat, groups = ind,
-                                       breaks = 100,
-                                       par.settings = par.settings, pch = '.',
-                                       xlab = xlab, ylab = ylab, main = main,
-                                       panel = panel.superpose,
-                                       panel.groups = function(x, group.value, col.line,...){
-                                           panel.densityplot(x,
-                                                             col.line = col.line,
-                                                             plot.points = FALSE,
-                                                             ...)
-                                           d <- density(x, na.rm = TRUE)
-                                           i <- which.max(d$y)
-                                           ltext(d$x[i],d$y[i],
-                                                 group.value,
-                                                 adj = c(0.3,0),
-                                                 col = col.line,
-                                                 cex = 0.7)
-                                       })
-                  
-                  else ##draw.labels = FALSE
-                      p <- densityplot(~values,
-                                       data = dat, groups = ind,
-                                       breaks = 100,
-                                       par.settings = par.settings, pch = '.',
-                                       xlab = xlab, ylab = ylab, main = main,
-                                       auto.key = auto.key, ...)                
-              } 
+              if (nlyr(x) > 1)
+                  densityMLayer(x,
+                                layers, FUN,
+                                maxpixels,
+                                xlab, ylab, main,
+                                par.settings, draw.labels, auto.key,
+                                att,
+                                ...)
               else
-              { ## Number of layers = 1
-                  dat <- raster2dat(x, maxpixels = maxpixels, att = att)
-                  p <- densityplot(dat,
-                                   data = NULL,
-                                   par.settings = par.settings,
-                                   pch = '.',
-                                   xlab = xlab, ylab = ylab, main = main,
-                                   ...) 
-              }
+                  densityLayer(x,
+                               maxpixels,
+                               xlab, ylab, main,
+                               par.settings, draw.labels, auto.key,
+                               att,
+                               ...)
+              
+          })
+
+########################################################################
+## densityplot for formula
+########################################################################
+
+setMethod('densityplot', signature(x = 'formula', data = 'Raster'),
+          definition = function(x, data, dirXY, maxpixels = 1e+05,
+            xscale.components = xscale.raster,
+            yscale.components = yscale.raster,
+            auto.key = list(space = 'right'), 
+            par.settings = rasterTheme(),...){
+
+              df <- dfRegular(data, maxpixels)
+
+              p <- densityplot(x = x, data = df,
+                               xscale.components = xscale.components,
+                               yscale.components = yscale.components,
+                               auto.key = auto.key, 
+                               par.settings = par.settings, ...)
               p
           }
           )
 
-                 
 
-setMethod('densityplot', signature(x='formula', data='RoT'),
-          definition=function(x, data, dirXY, maxpixels=1e+05,
-            xscale.components=xscale.raster,
-            yscale.components=yscale.raster,
+setMethod('densityplot', signature(x = 'formula', data = 'SpatRaster'),
+          definition = function(x, data, dirXY, maxpixels = 1e+05,
+            xscale.components = xscale.raster,
+            yscale.components = yscale.raster,
             auto.key = list(space = 'right'), 
-            par.settings=rasterTheme(),...){
+            par.settings = rasterTheme(),...){
 
+              df <- dfRegular(data, maxpixels)
 
-              if (is(data, "Raster"))
-              {
-                  dataSample <- sampleRegular(data,
-                                              size = maxpixels,
-                                              asRaster = TRUE)
-                  df <- as.data.frame(dataSample, xy = TRUE)
-                  
-              } else
-              {
-                  idx <- spatSample(data,
-                                    size = maxpixels,
-                                    cells = TRUE)
-                  vals <- data[idx]
-                  xy <- xyFromCell(data, idx)
-                  df <- cbind(xy, vals)
-              }
-
-            p <- densityplot(x = x, data = df,
-                             xscale.components = xscale.components,
-                             yscale.components = yscale.components,
-                             auto.key = auto.key, 
-                             par.settings = par.settings, ...)
-            p
+              p <- densityplot(x = x, data = df,
+                               xscale.components = xscale.components,
+                               yscale.components = yscale.components,
+                               auto.key = auto.key, 
+                               par.settings = par.settings, ...)
+              p
           }
           )
+
