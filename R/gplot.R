5441e0afe80007150da41690585b22680260f2bd
Oscar Perpiñán Lamigueiro <oscar.perpinan@upm.es>
2024-03-26 16:56:19 +0100

    More improvements to gplot

--- R/gplot.R
+++ R/gplot.R
@@ -5,8 +5,35 @@ if (!isGeneric("gplot")) {
         standardGeneric("gplot"))
 }	
 
+.gplot <- function(df, nly, isll, crs, palette, show.legend, ...)
+{
+    
+    p <- ggplot2::ggplot(...) +
+        ## Set CRS with crs and axis labels with datum (if
+        ## datum is not used, coordinates are transformed
+        ## and shown as lat-lon values)
+        ggplot2::coord_sf(crs = crs, datum = crs,
+                          expand = FALSE) +
+        ggplot2::geom_raster(data = df,
+                             ggplot2::aes(x = x, y = y,
+                                          fill = value),
+                             show.legend = show.legend)
+    if (is.character(palette))
+        p <- p + ggplot2::scale_fill_viridis_c(option = palette,
+                                               na.value="transparent")
+    else
+        p <- p + palette
+    if (isTRUE(isll))
+        p <- p + xlab("Longitude") + ylab("Latitude")
+    ## Multilayer objects
+    if (nly > 1)
+        p + ggplot2::facet_wrap(~ variable)
+    else
+        p
+}
+ 
 setMethod("gplot", signature(x='Raster'), 
-          function(x, maxpixels = 50000, palette = "magma", ...)
+          function(x, maxpixels = 50000, palette = "magma", show.legend = TRUE, ...)
           {
               if (!requireNamespace("ggplot2", quietly = TRUE))
               {
@@ -17,29 +44,22 @@ setMethod("gplot", signature(x='Raster'),
               dat <- stack(as.data.frame(raster::values(xs)))
               names(dat) <- c('value', 'variable')
               dat <- cbind(coords, dat)
-              ## wkt provides the CRS in the format required by coord_sf
-              mycrs <- raster::wkt(x)
-              p <- ggplot2::ggplot(...) +
-                  ## Set CRS with crs and axis labels with datum (if
-                  ## datum is not used, coordinates are transformed
-                  ## and shown as lat-lon values)
-                  ggplot2::coord_sf(crs = mycrs, datum = mycrs,
-                                    expand = FALSE) +
-                  ggplot2::geom_raster(data=dat,
-                                       ggplot2::aes(x = x, y = y,
-                                                    fill = value)) + 
-                  ggplot2::scale_fill_viridis_c(option = palette, na.value="transparent")
-              ## Multilayer objects
-              if (nlayers(x) > 1)
-                  p + ggplot2::facet_wrap(~ variable)
-              else
-                  p
+              
+              .gplot(df = dat,
+                     nly = raster::nlayers(x),
+                     isll = raster::isLonLat(x), ## Is longitude-latitude?
+                     crs = raster::wkt(x), ## wkt provides the CRS in
+                                           ## the format required by
+                                           ## coord_sf
+                     palette = palette,
+                     show.legend = show.legend,
+                     ...)
           }
           )
 
 
 setMethod("gplot", signature(x='SpatRaster'), 
-          function(x, maxpixels = 50000, palette = "magma", ...)  {
+          function(x, maxpixels = 50000, palette = "magma", show.legend = TRUE, ...)  {
               if (!requireNamespace("ggplot2", quietly = TRUE))
               {
                   stop("ggplot2 is required for the gplot method.")
@@ -49,18 +69,16 @@ setMethod("gplot", signature(x='SpatRaster'),
               dat <- stack(as.data.frame(terra::values(xs)))
               names(dat) <- c('value', 'variable')
               dat <- cbind(coords, dat)
-              mycrs <- terra::crs(x)
-              p <- ggplot2::ggplot(...) +
-                  ggplot2::coord_sf(crs = mycrs, datum = mycrs,
-                                    expand = FALSE) + 
-                  ggplot2::geom_raster(data = dat,
-                                       ggplot2::aes(x = x, y = y,
-                                                    fill = value)) +
-                  ggplot2::scale_fill_viridis_c(option = palette, na.value="transparent")
-              if (nlyr(x) > 1)
-                  p + ggplot2::facet_wrap(~ variable)
-              else
-                  p
+              
+              .gplot(df = dat,
+                     nly = terra::nlyr(x),
+                     isll = terra::is.lonlat(x), ## Is longitude-latitude?
+                     crs = terra::crs(x), ## wkt provides the CRS in
+                                           ## the format required by
+                                           ## coord_sf
+                     palette = palette,
+                     show.legend = show.legend,
+                     ...)
           }
           )
 
